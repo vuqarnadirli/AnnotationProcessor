bbc196ddf9d8585c63b41d8c190094419f94f8ba
Vuqar <nadirlivuqar@gmail.com>
2023-01-31 23:50:28 +0400

    Reflection getDeclaredMethods();

--- src/Test.java
+++ src/Test.java
@@ -5,10 +5,10 @@ public class Test {
     public static void main(String[] args) {
         Teacher t = new Teacher();
 
-        Method[] methods = t.getClass().getMethods();
+        Method[] methods = t.getClass().getDeclaredMethods();
         for (int i = 0; i < methods.length; i++) {
             System.out.println(methods[i].getName());
         }
-    }
 
+    }
 }
